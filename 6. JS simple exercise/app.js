ec8fc6bda83c32aeed3cb242fd50b275a7bb2dd5
Marta Łoskot <loskotmarta@gmail.com>
2024-09-21 17:45:55 +0200

    Add simple JS task

---
+++ 6. JS simple exercise/app.js
@@ -0,0 +1,18 @@
+// 3. Zadanie z JavaScript
+// Opis: Napisz funkcję, która przyjmuje tablicę liczb i zwraca nową tablicę, w której każda liczba jest podniesiona do kwadratu.
+// Użyj metod tablicowych (map, filter).
+// Zidentyfikuj i obsłuż potencjalne błędy (np. co zrobić, jeśli tablica zawiera wartości niebędące liczbami).
+// ! (operator negacji): Operator negacji ! zmienia wynik metody every(). Jeśli every() zwraca true (co oznacza, że wszystkie elementy są liczbami), to !true daje false. Jeśli natomiast every() zwraca false (co oznacza, że przynajmniej jeden element nie jest liczbą), to !false daje true.
+
+// Całe wyrażenie:
+// !numbers.every(num => typeof num === 'number'): To wyrażenie zwraca true, jeśli co najmniej jeden element w tablicy numbers nie jest liczbą, a false, jeśli wszystkie są liczbami.
+
+const ourFunction = (numbers) => {
+    if (numbers.length === 0 || !numbers.every(num => typeof num === 'number'))
+        return "Sorry, it must be a number in here"
+    return numbers.map(number =>  number*number)
+}
+
+const numbers = [1,2,3,4,5,6,7]
+
+console.log(ourFunction(numbers))
