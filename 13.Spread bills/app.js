57b36a3e1dc63557e56ec8e3b4f6e2d864003890
Marta Łoskot <loskotmarta@gmail.com>
2024-11-20 13:02:18 +0100

    Making some progress

--- 13.Spread bills/app.js
+++ 13.Spread bills/app.js
@@ -1,18 +1,5 @@
 //A group of friends went on a trip. Each person paid individually for various things on the trip; some people paid more, some people paid less. After they came back, they wanted to split the costs evenly.Implement a solution to calculate a list of payment transactions to clear the outstanding debts.
 
-const divideExpenses = (expenses) => {
-//1.Zliczyć ile jest osób
-const howManyPersons = expenses.length
-//2.Policzyć średnia wydatków
-console.log(expenses.person)
-//const wholeAmount = 
-//const averageAmount = wholeAmount/howManyPersons
-//3.Zobaczyć, kto jest poniżej a kto rowno lub powyżej tych wydatków
-//4.Utwożyć dwie listy z tymi osobami
-
-}
-
-
 const expenses = [
   {
     person: 'Alice',
@@ -27,11 +14,32 @@ const expenses = [
     amount: 10,
   }
 ]
+console.log(expenses[1].amount)
+
+const divideExpenses = (expenses) => {
+  const howManyPersons = expenses.length
+  let wholeAmount = 0
+  for (let i = 0; i < howManyPersons; i++) {
+    wholeAmount += expenses[i].amount
+  }
+  const averageAmount = wholeAmount / howManyPersons
+
+
+  return transactions = [
+    {
+      from: ,
+      to: ,
+      amount:
+    }
+  ]
+}
+
 console.log(divideExpenses(expenses))
 
-// Output:transactions = [
-//   {
-//     from: 'Mike',
-//     to: 'Bob',
-//     amount: 10,
-//   }
+
+
+// {
+//   from: 'Mike',
+//   to: 'Bob',
+//   amount: 10,
+// }
