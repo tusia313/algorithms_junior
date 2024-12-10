888b605167827df921aaa44facd5cfd4a1d0bddd
Marta Łoskot <loskotmarta@gmail.com>
2024-12-10 20:15:42 +0100

    creating new task

---
+++ 17.Zliczenie liczby wystąpień/app.js
@@ -0,0 +1,19 @@
+// Masz tablicę obiektów reprezentujących produkty. Twoim zadaniem jest napisać funkcję, która zwróci obiekt, w którym klucze to nazwy kategorii produktów, a wartości to liczba wystąpień tych kategorii.
+const products = [
+    { name: "Laptop", category: "Electronics" },
+    { name: "Phone", category: "Electronics" },
+    { name: "Shirt", category: "Clothing" },
+    { name: "Pants", category: "Clothing" },
+    { name: "Keyboard", category: "Electronics" },
+    { name: "Shoes", category: "Clothing" }
+]
+
+const numberOfProducts = (products) => {
+
+}
+console.log(numberOfProducts(products))
+// {
+//     Electronics: 3,
+//     Clothing: 3
+// }
+
